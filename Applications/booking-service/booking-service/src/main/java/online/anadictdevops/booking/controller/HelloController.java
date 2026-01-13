9c264db0e70b838c1e5c87263576405c67804432
Anadict Mathew <nadictmgmail.com>
2026-01-13 08:54:30 +0530

    Added MicroServices Application

---
+++ Applications/booking-service/booking-service/src/main/java/online/anadictdevops/booking/controller/HelloController.java
@@ -0,0 +1,13 @@
+package online.anadictdevops.booking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Booking Service is running with Java 21!";
+    }
+}
---
+++ Applications/logging-service/logging-service/src/main/java/logging/anadictdevops/online/controller/LogController.java
@@ -0,0 +1,13 @@
+package logging.anadictdevops.online.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogController {
+
+    @GetMapping("/log")
+    public String log() {
+        return "Logging service is running!";
+    }
+}
